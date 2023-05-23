86ac6685916879fd6a22e2e9c3e70f002816d407
Zelealem Gebru <123925020+zelealemgebru@users.noreply.github.com>
2023-05-22 22:39:36 -0700

    Add files via upload

---
+++ Block 21 Workshop Car Factory/index.js
@@ -0,0 +1,41 @@
+class car {
+  contructore(make, model, year) {
+         getDescription.make = make;
+         getDescription.model = model;
+         getDescription.year = yaer;                        
+              }
+getDescription() {
+      return `This car has ${getDescription.make} make, ${getDescription.model} model, and is ${getDescription.year} year.`;
+    }
+}
+class ElectricCar extends car {
+              contructore(make, model, year, range) {
+                           super(make, model, year)
+                            getDescription.range = range;
+}
+getDescription() {
+return `${super.getDesc()}It has a ${getDescription.range} mileage.`;}
+}
+
+
+ getDesc() 
+              return `This car has ${getDesc.make} make, ${getDesc.model} model, ${getDesc.year} year and is ${getDesc.range} range.`;
+         
+           
+              ElectricCar = getDescElectricCar("Tesla", "Model S", 2019, 300);
+              description = electricCar.getDescription();
+           
+
+   class ElectricCar {
+              contractore(make, model, year, range){
+                 getDescriptionc.make = "Tesla"
+                 getDescription.model = "Model S"
+                 getDescription.year = 2019
+                 getDescription.range = 300
+                 }
+              }
+
+{
+console.log();System.out.print(Car);
+console.log();System.out.print(ElectricCar);
+}
